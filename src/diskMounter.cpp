79935b19e872b6e9f604b9f492463e3cb1248b74
CheezeCake <emmanuel.nicolet@gmail.com>
2016-04-24 18:51:45 +0200

    dialogs n shiet

--- src/diskMounter.cpp
+++ src/diskMounter.cpp
@@ -1,14 +1,23 @@
+#include <iostream>
+
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include <QIcon>
+#include <QInputDialog>
+#include <QMessageBox>
 
 #include "diskMounter.hpp"
+
+const char* DiskMounter::MountUtility = "/sbin/mount";
+const char* DiskMounter::UmountUtility = "/sbin/umount";
 
 DiskMounter::DiskMounter(QObject* parent) :
 	QObject(parent), menu(new QMenu(nullptr)),
 	sysTrayIcon(new QSystemTrayIcon(nullptr)),
 	diskWatcher(DiskWatcher::getInstance())
 {
+	mountedSeparatorAction = menu->addSeparator();
 	quitSeparatorAction = menu->addSeparator();
 	QAction* quit = menu->addAction("Quit");
 	QObject::connect(quit, SIGNAL(triggered(bool)), this,
@@ -17,8 +26,8 @@ DiskMounter::DiskMounter(QObject* parent) :
 	sysTrayIcon->setContextMenu(menu.get());
 	sysTrayIcon->setIcon(QIcon(":/images/hdd.png"));
 
-	QObject::connect(&diskWatcher, SIGNAL(onDiskAppeared(QString, qint64)), this,
-			SLOT(onDiskAppeared(QString, qint64)));
+	QObject::connect(&diskWatcher, SIGNAL(onDiskAppeared(QString, qint64)),
+			this, SLOT(onDiskAppeared(QString, qint64)));
 	QObject::connect(&diskWatcher, SIGNAL(onDiskDisappeared(QString)), this,
 			SLOT(onDiskDisappeared(QString)));
 }
@@ -34,45 +43,138 @@ void DiskMounter::runDiskWatcher()
 	diskWatcher.run();
 }
 
-void DiskMounter::onDiskClicked(QString deviceNodePath)
+bool DiskMounter::execCmd(const char* cmd, const std::vector<char*>& cmdArgs)
 {
-	QAction* action = devices.at(deviceNodePath);
-	bool enabled = action->isEnabled();
+	const pid_t child = fork();
 
-	if (enabled) {
-		pid_t child = fork();
-		if (child == -1) {
-			perror("fork");
-			return;
-		}
-
-		if (child == 0) {
-			QDir mountPoint = QDir::homePath();
-			mountPoint.mkdir("./usb");
-			mountPoint.cd("./usb");
-
-			printf("mntPt absolute path : %s\n", mountPoint.absolutePath().toStdString().c_str());
-
-			execl("/sbin/mount", "/sbin/mount", "-t", "msdos",
-					deviceNodePath.toStdString().c_str(),
-					mountPoint.absolutePath().toStdString().c_str(), nullptr);
-			perror("/sbin/mount");
-		}
+	if (child == -1) {
+		perror("fork");
+		return false;
 	}
 
-	action->setEnabled(!enabled);
+	if (child == 0) {
+		execv(cmd, cmdArgs.data());
+		perror(cmdArgs.front());
+		exit(1);
+	}
+	else {
+		int status;
+		wait(&status);
+
+		return (status == 0);
+	}
+}
+
+void DiskMounter::runUtility(Device& device, const char* utility,
+		const std::vector<char*>& execArgs, const QString& successMessage,
+		const QString& errorMessage, bool mounted, const QString& mountPoint,
+		QAction* before)
+{
+	// disable the menu action during the mount operation
+	device.action->setEnabled(false);
+
+	if (execCmd(utility, execArgs)) {
+		QMessageBox::information(nullptr, utility, successMessage);
+
+		// put the device in the "mounted" section of the menu
+		QString actionText = device.action->text();
+		menu->removeAction(device.action);
+
+		device.action = insertDiskAction(device.deviceNodePath, before,
+				actionText);
+		device.mounted = mounted;
+		device.mountPoint = mountPoint;
+	}
+	else {
+		QMessageBox::warning(nullptr, utility, errorMessage);
+
+		device.action->setEnabled(true);
+	}
+}
+
+void DiskMounter::mountDisk(Device& device)
+{
+	if (device.mounted) {
+		std::cerr << device.deviceNodePath.toStdString() << " already mounted on : "
+			<< device.mountPoint.toStdString() << '\n';
+		return;
+	}
+
+	const QString fsType = QInputDialog::getText(nullptr, "fs type", "fs type",
+			QLineEdit::Normal, "msdos");
+	const QString mountPoint = QFileDialog::getExistingDirectory(nullptr,
+			"mount point", QDir::homePath());
+	const std::vector<char*> execArgs = {
+		const_cast<char*>(MountUtility),
+		const_cast<char*>("-t"),
+		const_cast<char*>(fsType.toStdString().c_str()),
+		const_cast<char*>(device.deviceNodePath.toStdString().c_str()),
+		const_cast<char*>(mountPoint.toStdString().c_str()),
+		nullptr
+	};
+
+	runUtility(device, MountUtility, execArgs,
+				device.deviceNodePath + " successfully mounted on " + mountPoint,
+				device.deviceNodePath + " mount on " + mountPoint + " failed!",
+				true, mountPoint, quitSeparatorAction);
+}
+
+void DiskMounter::unmountDisk(Device& device)
+{
+	if (!device.mounted) {
+		std::cerr << device.deviceNodePath.toStdString() << " not mounted\n";
+		return;
+	}
+
+	const std::vector<char*> execArgs = {
+		const_cast<char*>(UmountUtility),
+		const_cast<char*>(device.mountPoint.toStdString().c_str()),
+		nullptr
+	};
+
+	runUtility(device, UmountUtility, execArgs,
+				device.deviceNodePath + " successfully unmounted from " +
+				device.mountPoint,
+				device.deviceNodePath + " unmount from " + device.mountPoint +
+				" failed!",
+				false, "", mountedSeparatorAction);
+}
+
+void DiskMounter::onDiskClicked(QString deviceNodePath)
+{
+	auto& device = devices.at(deviceNodePath);
+
+	if (device.action->isEnabled()) {
+		if (device.mounted) {
+			if (QMessageBox::question(nullptr, "DiskMounter",
+						"Unmount " + deviceNodePath + " ?",
+						QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes)
+				unmountDisk(device);
+		}
+		else {
+			mountDisk(device);
+		}
+	}
+}
+
+QAction* DiskMounter::insertDiskAction(const QString& deviceNodePath,
+		QAction* before, const QString& text)
+{
+	QAction* deviceAction = menu->insertSection(before, text);
+	QObject::connect(deviceAction, &QAction::triggered, [this, deviceNodePath] {
+			onDiskClicked(deviceNodePath);
+			});
+	deviceAction->setSeparator(false);
+
+	return deviceAction;
 }
 
 void DiskMounter::onDiskAppeared(QString deviceNodePath, qint64 size)
 {
 	if (devices.find(deviceNodePath) == devices.end()) {
-		QAction* deviceAction = menu->insertSection(quitSeparatorAction,
-				createDiskLabel(deviceNodePath, size));
-		QObject::connect(deviceAction, &QAction::triggered, [this, deviceNodePath] {
-					onDiskClicked(deviceNodePath);
-				});
-		deviceAction->setSeparator(false);
-		devices.emplace(deviceNodePath, deviceAction);
+		devices.emplace(deviceNodePath, Device(deviceNodePath,
+					insertDiskAction(deviceNodePath, mountedSeparatorAction,
+						createDiskLabel(deviceNodePath, size))));
 	}
 }
 
@@ -80,8 +182,8 @@ void DiskMounter::onDiskDisappeared(QString deviceNodePath)
 {
 	const auto it = devices.find(deviceNodePath);
 	if (it != devices.end()) {
+		menu->removeAction(it->second.action);
 		devices.erase(it);
-		menu->removeAction(it->second);
 	}
 }
 
--- src/diskMounter.hpp
+++ src/diskMounter.hpp
@@ -16,22 +16,53 @@ class DiskMounter : public QObject
 {
 	Q_OBJECT
 
+	public:
+		struct Device
+		{
+			QString deviceNodePath;
+			QString mountPoint;
+			QAction* action;
+			bool mounted;
+
+			Device(const QString& nodePath, QAction* act) :
+				deviceNodePath(nodePath), action(act), mounted(false)
+			{
+			}
+		};
+
+		static const char* MountUtility;
+		static const char* UmountUtility;
+
 	private:
 		std::unique_ptr<QMenu> menu;
 		std::unique_ptr<QSystemTrayIcon> sysTrayIcon;
 
 		DiskWatcher& diskWatcher;
 		std::thread diskWatcherThread;
+		QAction* mountedSeparatorAction;
 		QAction* quitSeparatorAction;
-		std::map<QString, QAction*> devices;
+		std::map<QString, Device> devices;
 
 		void runDiskWatcher();
 		void onDiskClicked(QString deviceNodePath);
 
-		static inline QString createDiskLabel(QString deviceNodePath, qint64 size)
+		static inline QString createDiskLabel(QString deviceNodePath,
+				qint64 size)
 		{
-			return (deviceNodePath + " (" + QString::number(size / (1024 * 1024 * 1024)) + " GiB)");
+			return (deviceNodePath + " (" +
+					QString::number(size / (1024 * 1024 * 1024)) + " GiB)");
 		}
+
+		QAction* insertDiskAction(const QString& deviceNodePath,
+				QAction* before, const QString& text);
+		void runUtility(Device& device, const char* utility,
+				const std::vector<char*>& execArgs,
+				const QString& successMessage, const QString& errorMessage,
+				bool mounted, const QString& mountPoint, QAction* before);
+
+		bool execCmd(const char* cmd, const std::vector<char*>& cmdArgs);
+		void mountDisk(Device& device);
+		void unmountDisk(Device& device);
 
 	private slots:
 		void onQuitClicked(bool checked = false);
